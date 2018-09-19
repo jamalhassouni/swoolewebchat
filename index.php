0ab5a61aed8bbded2ac0b66637c588abacec46b1
jamal hassouni <jamalhassouni13@gmail.com>
2018-09-20 00:08:43 +0100

    refactor:Change application design

--- index.php
+++ index.php
@@ -1,64 +1,35 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
         <title>Swoole web chat</title>
         <!-- bootstrap -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
         <!-- bootstrap -->
+        <link rel="stylesheet" href="css/styles.css">
     </head>
         <body>
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <br>
-                        <div class="well" id="messagebox" style="min-height: 200px;overflow: scroll"></div>
-                        <br>
-                        <textarea name="" id="message" cols="30" rows="3" class="form-control"></textarea>
-                        <br>
-                         <div class="form-group">
-                            <input type="submit" onclick="sendMessage();return false;" name="submit" value="Submit" class="btn btn-default col-lg-12" />
-                         </div>
-                    </div>
+            <div class="chat_window">
+              <div class="top_menu">
+                <div class="buttons">
+                  <div class="button close">
+                  </div><div class="button minimize">
+                  </div><div class="button maximize">
+                  </div>
                 </div>
+                <div class="title">Chat</div>
+              </div>
+              <ul class="messages" id="messagesBox"></ul>
+              <div class="bottom_wrapper clearfix">
+              <div class="message_input_wrapper">
+              <input class="message_input" id="message" placeholder="Type your message here..." />
             </div>
-
-            <script>
-                var exampleSocket = new WebSocket("ws://localhost:8080");
-
-                exampleSocket.onopen = function(event){
-                    console.log("user connected");
-                };
-
-                 sendMessage = () => {
-                    let messageElem = document.getElementById('message');
-                    let textMessage = messageElem.value;
-
-                    let msg = {
-                        type: "message",
-                        text : textMessage,
-                        date : Date.now()
-                    };
-
-                    exampleSocket.send(JSON.stringify(msg));
-
-                  messageElem.value = "";
-
-                    messageParse(textMessage);
-
-                }
-
-                exampleSocket.onmessage = function (event) {
-                    messageParse(JSON.parse(event.data).text)
-                }
-
-                 messageParse = (text) => {
-                  document.getElementById('messagebox').innerHTML += `<p>${text}</p>`;
-                }
-
-
-            </script>
+            <div onclick="sendMessage();return false;" class="send_message">
+            <div class="icon"></div>
+            <div  class="text">Send</div>
+           </div>
+         </div>
+       </div>
+    <script src="js/main.js"></script>
 
         </body>
 </html>
