a3a7891f332ca910f0d81c7bf9f13140595e0e7e
jamal hassouni <jamalhassouni13@gmail.com>
2018-09-19 13:33:11 +0100

    refactor: Create swoole Server

---
+++ server.php
@@ -0,0 +1,23 @@
+<?php
+$server = new swoole_websocket_server("localhost", 8080);
+
+// events
+  // open
+$server->on("open",function (swoole_websocket_server $server,$request ){
+echo "User ".$request->fd." connect\n";
+});
+
+  // message
+
+  $server->on("message",function (swoole_websocket_server $server,$frame ){
+  echo "User ".$frame->fd." send message ".$frame->data ." \n";
+  });
+
+  // close
+  $server->on("close",function($server,$fd ){
+  echo "User ".$fd." disconnet\n";
+  });
+  // start
+
+  $server->start();
+?>
